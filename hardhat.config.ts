88d67342d68f2e0957ad38c0f2616e5b9530bae9
Melih Gunduz <melih.gunduz@hotmail.com>
2023-09-22 16:13:55 +0300

    ready for deploy and verify

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+const {MNEMONIC, BSCAPIKEY} = require('./secrets.json');
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   networks: {
     hardhat: {
     },
-    // bscTestnet: {
-
-    // }
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.bnbchain.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: MNEMONIC},
+    },
+  },
+  etherscan: {
+    apiKey: BSCAPIKEY
   }
 };
 
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,13 +1,10 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const journeyToken = await ethers.deployContract("JourneyToken");
+  const journeyToken = await ethers.deployContract("JourneyToken"); //defining our contract
 
-  await journeyToken.waitForDeployment();
-  const [owner] = await ethers.getSigners();
-  console.log(
-    `${owner.address} deployed contract to ${journeyToken.target}`
-  );
+  await journeyToken.waitForDeployment(); // deploying our contract on network
+  console.log(await journeyToken.getAddress()) // writing contract address to the console
 }
 
 // We recommend this pattern to be able to use async/await everywhere
